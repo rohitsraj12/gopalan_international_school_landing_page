cda9623f589b6a232e8f519834c8b567f334d63b
Rohit <rohitsraj12@gmail.com>
2020-12-17 17:49:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,23 @@ $(document).ready(function(){
     // hamberger toggle
     $(".hamberger").click(function(){
         $(".header__nav").slideToggle();
-    })
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 200) {
+            $("#body-header").addClass("header-fixed");
+            $("#body-header header").removeClass("container");
+        } else {
+            $("#body-header header").addClass("container");
+            $("#body-header").removeClass("header-fixed");
+        }
+
+        if(scroll >= 200){
+          $("#fixed-button").removeClass("hide");
+        } else {
+          $("#fixed-button").addClass("hide");
+        }
+    });
 })
